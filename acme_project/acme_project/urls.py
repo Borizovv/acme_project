88b107a949178a4c11992da0670a6ef572e0d50a
Vasiliy Borisov <borisov1983@gmail.com>
2024-05-06 14:09:43 +0900

    acme project for learning

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -1,8 +1,43 @@
+# acme_project/urls.py
+# Импортируем настройки проекта.
+from django.conf import settings
+# Импортируем функцию, позволяющую серверу разработки отдавать файлы.
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+
+# классы для создания пользователей прямо в урлс
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
+from django.urls import include, path, reverse_lazy
 
 urlpatterns = [
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
     path('birthday/', include('birthday.urls')),
+    # Подключаем urls.py приложения для работы с пользователями.
+    path('auth/', include('django.contrib.auth.urls')),
+    # сразу в path, перадаем в аргументы в метод as_view, для создания
+    # пользователей без необходимости писать новый CBV в views.py
+    path(
+        'auth/registration/',
+        CreateView.as_view(
+            # указываем форму
+            template_name='registration/registration_form.html',
+            # встроенный класс для создания пользователей
+            form_class=UserCreationForm,
+            # путь для редиректа после аутентификации
+            success_url=reverse_lazy('pages:homepage'),
+        ),
+        name='registration',
+    ),
+    # В конце добавляем к списку вызов функции static.
 ]
+
+handler404 = 'core.views.page_not_found'
+
+if settings.DEBUG:
+    import debug_toolbar
+# Добавить к списку urlpatterns список адресов из приложения debug_toolbar:
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- acme_project/birthday/admin.py
+++ acme_project/birthday/admin.py
@@ -1,1 +1,9 @@
 from django.contrib import admin
+
+
+# Из модуля models импортируем модель
+from .models import Birthday, Tag
+
+APP_MODELS = (Birthday, Tag)
+admin.site.register(APP_MODELS)
+# admin.site.register(Tag)
